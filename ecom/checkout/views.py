6f4b0568a5f49cce23155b97c318613ffd42746c
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-15 21:46:11 +0000

    Stripe setup

--- ecom/checkout/views.py
+++ ecom/checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OjcqcLRHDoZQqqVEddcjfqL9QUy506aDastjCbk6mc2yRQbilCqWI6aiHukeJA4TUyB5Ri0mqoFlbDbbbdSGscx00swMhPC9U',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
