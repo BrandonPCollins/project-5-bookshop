5abe5c43e34b5cdaf10fd4477cb982525f26e4ee
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-17 01:43:02 +0000

    Update wsgi.py

--- ecom/ecom/wsgi.py
+++ ecom/ecom/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import django.core.handlers.wsgi
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+application = django.core.handlers.wsgi.WSGIHandler()
