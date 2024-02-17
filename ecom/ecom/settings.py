97b0efc69cbd02836408ec86e2061fd572b5f87d
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-17 01:03:27 +0000

    Heroku preparation

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -1,7 +1,9 @@
 from pathlib import Path
-import os 
+import os
+import dj_database_url
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -18,7 +20,7 @@ SECRET_KEY = 'django-insecure-((ct^dv8g1@3t0o0)y4$*q-a4uey8e^$z%4bbpq(ohy5)*lf0t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',]
 
 
 # Application definition
@@ -116,12 +118,17 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
