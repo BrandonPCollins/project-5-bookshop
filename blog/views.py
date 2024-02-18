48f32a676c7c0f48f0d8c7224c77372f590db6ab
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-18 00:48:03 +0000

    Comments limited to users

--- blog/views.py
+++ blog/views.py
@@ -10,15 +10,17 @@ def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     comments = post.comments.order_by('-created_date')
     new_comment = None
+    comment_form = None
 
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.author = request.user
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            comment_form = CommentForm(data=request.POST)
+            if comment_form.is_valid():
+                new_comment = comment_form.save(commit=False)
+                new_comment.post = post
+                new_comment.author = request.user
+                new_comment.save()
+        else:
+            comment_form = CommentForm()
 
     return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
