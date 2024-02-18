528493733201fe13d65ca90f4f9184bcad336568
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-18 09:41:59 +0000

    Remove author choice on blog page

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'author', 'image')
+        fields = ('title', 'content', 'image')
         widgets = {
             'image': forms.FileInput(),
         }
