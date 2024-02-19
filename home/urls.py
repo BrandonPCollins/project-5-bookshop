1ca3e0e78ec5274169a28eda21b7c81c5579e14f
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-19 07:56:58 +0000

    Added featured products to index

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from products.views import product_detail
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('products/<int:product_id>/', product_detail, name='product_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
     """" A view that displays the index page """
-    return render(request, 'home/index.html')
+    featured_products = Product.objects.filter(featured=True)
+    return render(request, 'home/index.html', {'featured_products': featured_products})
