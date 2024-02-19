4f5570d457b9bfa3180eed8b22f8cde6e8a70c15
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-19 08:57:07 +0000

    About Us & FAQ Page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from products.views import product_detail
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
     path('products/<int:product_id>/', product_detail, name='product_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ def index(request):
     """" A view that displays the index page """
     featured_products = Product.objects.filter(featured=True)
     return render(request, 'home/index.html', {'featured_products': featured_products})
+
+def about(request):
+    """" A view that displays the about page """
+    return render(request, 'home/about.html')
