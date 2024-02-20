4aa3e7e3627319f44dac561f87f3ef9f1958e1dc
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-20 09:56:01 +0000

    Basic Like Comment Functionality

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='post_images/', null=True, blank=True)
     def __str__(self):
         return self.title
-    
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -23,3 +23,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author}'
+
+    def is_liked_by_user(self, user):
+        return self.likes.filter(user=user).exists()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        unique_together = ('user', 'comment')
+
+    def __str__(self):
+        return f'Like by {self.user} on {self.comment}'
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_list, post_detail, delete_comment, create_post
+from .views import post_list, post_detail, delete_comment, create_post, like_comment
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('comment/<int:comment_id>/delete/', delete_comment, name='delete_comment'),
     path('edit_comment/<int:comment_id>/', views.edit_comment, name='edit_comment'),
     path('create_post/', create_post, name='create_post'),
+    path('like_comment/', like_comment, name='like_comment'),
     ]
