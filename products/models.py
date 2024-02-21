b49be874c0e4f785b6bf5bace5415fc4785bb0c7
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-21 17:00:59 +0000

    Added Product Author to Product model

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=255)
     friendly_name = models.CharField(max_length=255, null=True, blank=True)
 
@@ -19,6 +19,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    author = models.CharField(max_length=200, default='Unknown')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -27,7 +28,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
