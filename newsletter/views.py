8fe61f032f29e9ebad236a738021fc58fd604839
BrandonPCollins <bpcollins1995@gmail.com>
2024-02-21 00:43:14 +0000

    Newsletter Confirmation Email when signing up

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
 from .forms import NewsletterSubscriptionForm
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_confirmation_email(email):
+    subject = 'Newsletter Subscription Confirmation'
+    message = 'Thank you for subscribing to our newsletter!'
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = [email]
+    send_mail(subject, message, email_from, recipient_list)
 
 def subscribe_newsletter(request):
     if request.method == 'POST':
         form = NewsletterSubscriptionForm(request.POST)
         if form.is_valid():
             form.save()
+            send_confirmation_email(form.cleaned_data.get('email'))
             return redirect('newsletter:subscription_success')
     else:
         form = NewsletterSubscriptionForm()
